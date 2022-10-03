c4e47bd7b2e5a878915fc0a1a87d3e00ff512aea
lex-rutovich <110100770+lex-rutovich@users.noreply.github.com>
2022-10-03 18:22:16 +0300

    commit 500

--- Program.cs
+++ Program.cs
@@ -1,8 +1,18 @@
+// var builder = WebApplication.CreateBuilder(args);
+// var app = builder.Build();
+
+// app.MapGet("/", () => "Hello World!");
+
+// app.Run();
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddMvc();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
 
+app.UseEndpoints(e 
+{
+    e.MapControllerRoute("default", "{controller}/{faction}");
+});
 app.Run();
-
-
